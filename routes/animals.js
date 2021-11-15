01ba11dbf6c0453a6d1b86186bbaedd00416b314
9UqUaGyYYdMDaqdL8QZdxrzJIIHT3 <mjhiggins2@loyola.edu>
2021-11-14 19:55:02 -0500

    fully functional mongoDB

--- routes/animals.js
+++ routes/animals.js
@@ -2,5 +2,28 @@ const express= require('express')
 const router= express.Router()
 
 //Getting data from API
+router.get('/',(req,res) => {
+    res.send('Hello World!')
 
+
+})
+router.get('/:id',(req,res) =>{
+    
+    res.send(req.params.id)
+})
+//Sending data to the API
+router.post('/', (req,res) => {
+    
+
+})
+//Updating Data from the API
+router.patch('/:id', (req,res) => {
+    
+    
+})
+//Deleting Data from the API
+router.delete('/:id', (req,res) => {
+    
+    
+})
 module.exports = router
